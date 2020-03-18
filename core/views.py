07fdf6b4f8d206bc9f89974180f484620605a899
luisparf <luispauloaraujoafonso@gmail.com>
2020-03-18 17:39:52 -0300

    second

--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+from .forms import ContatoForm
+
 def index(request):
     return render(request, 'index.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
 
 def produto(request):
     return render(request, 'produto.html')
