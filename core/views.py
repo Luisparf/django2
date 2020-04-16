6644a6137c05a6e5be117a489ef5c8d9bfa2ef11
luisparf <luispauloaraujoafonso@gmail.com>
2020-04-16 20:13:34 -0300

    update form

--- core/views.py
+++ core/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.contrib import messages  # import para messages.sucess
 from .forms import ContatoForm, ProdutoModelForm
-
+from .models import Produto
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'produtos': Produto.objects.all()
+    }
+    return render(request, 'index.html', context)
 
 
 def contato(request):
